e36154d3a419e5d5aee078624f16116df49f19a4
Shikiya67 <princegabrielsanandres@gmail.com>
2026-08-18 07:18:45 +0800

    Add MalSync object with media start functionality

---
+++ application/vlc-android/src/org/videolan/vlc/MalSync.kt
@@ -0,0 +1,10 @@
+package org.videolan.vlc
+
+object MalSync {
+
+    fun onMediaStarted(fileName: String) {
+        // TODO: detect anime title + season + episode
+        // TODO: search MAL
+        // TODO: mark episode as watching
+    }
+}
