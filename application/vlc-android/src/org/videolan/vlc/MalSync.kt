4e1959fa18c12496d694c7924f0783a2ba8fd74e
Shikiya67 <princegabrielsanandres@gmail.com>
2026-08-18 07:21:42 +0800

    Implement media start logging in MalSync
    Added logging for media start with file name extraction.

--- application/vlc-android/src/org/videolan/vlc/MalSync.kt
+++ application/vlc-android/src/org/videolan/vlc/MalSync.kt
@@ -3,8 +3,8 @@ package org.videolan.vlc
 object MalSync {
 
     fun onMediaStarted(fileName: String) {
-        // TODO: detect anime title + season + episode
-        // TODO: search MAL
-        // TODO: mark episode as watching
+        val name = fileName.substringBeforeLast(".")
+
+        println("MAL SYNC: Media started: $name")
     }
 }
